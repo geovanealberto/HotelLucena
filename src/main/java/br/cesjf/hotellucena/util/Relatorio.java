0efc75f9ca3b01d37dbb2d3bc00569053a840945
geovanealberto <geovanealbertodepaula@hotmail.com>
2020-05-24 20:15:12 -0300

    Relatorios

---
+++ src/main/java/br/cesjf/hotellucena/util/Relatorio.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.cesjf.hotellucena.util;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletResponse;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.util.JRLoader;
+
+/**
+ *
+ * @author lucas
+ */
+public class Relatorio {
+    
+    private HttpServletResponse response;
+    private FacesContext context;
+    private ByteArrayOutputStream baos;
+    private InputStream stream;
+    private Connection con;
+
+    public Relatorio() {
+        this.context = FacesContext.getCurrentInstance();
+        this.response = (HttpServletResponse) context.getExternalContext().getResponse();
+    }
+   
+    public void getRelatorio(String relatorio){
+        
+        stream = this.getClass().getResourceAsStream("/"+relatorio+".jasper");
+        Map<String, Object> params = new HashMap<String, Object>();
+
+        baos = new ByteArrayOutputStream();
+        
+        try {
+            
+            JasperReport report = (JasperReport) JRLoader.loadObject(stream);
+            
+            JasperPrint print = JasperFillManager.fillReport(report, params, getConexao());
+            JasperExportManager.exportReportToPdfStream(print, baos);
+            
+            response.reset();
+            response.setContentType("application/pdf");
+            response.setContentLength(baos.size());
+            response.setHeader("Content-disposition", "inline; filename="+relatorio+".pdf");
+            response.getOutputStream().write(baos.toByteArray());
+            response.getOutputStream().flush();
+            response.getOutputStream().close();
+            
+            context.responseComplete();
+            fecharConexao();
+            
+        } catch (JRException ex) {
+            Logger.getLogger(Relatorio.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Relatorio.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }    
+    
+    public Connection getConexao(){        
+        try {            
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("java:app/jdbc:mysql://localhost:3306/HotelLucena?zeroDateTimeBehavior=convertToNull", "root", "");
+            return con;
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(Relatorio.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Relatorio.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return con;
+    }
+    
+    public void fecharConexao(){
+        try {
+            con.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(Relatorio.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+}
